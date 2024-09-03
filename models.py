1eab908ffe6ea27c1dba6164fad6fce0a9dabc34
thamiwinner <107689141+thamiwinner@users.noreply.github.com>
2024-09-03 21:19:12 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+# reporters/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ views.py
@@ -0,0 +1,28 @@
+
+from django.shortcuts import render
+
+def welcome_view(request):
+    return render(request, 'reporters/welcome.html')
+
+# reporters/views.py
+from django.shortcuts import render
+
+def register_view(request):
+    return render(request, 'reporters/register.html')
+
+def login_view(request):
+    return render(request, 'reporters/login.html')
+
+# reporters/views.py
+from django.shortcuts import render
+
+def welcome_view(request):
+    return render(request, 'reporters/welcome.html')
+
+def register_view(request):
+    return render(request, 'reporters/register.html')
+
+def login_view(request):
+    return render(request, 'reporters/login.html')
+
+
